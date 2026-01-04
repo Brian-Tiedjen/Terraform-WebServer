0849b9f3872f1bf1129718c63d0912f3e290a248
Brian-Tiedjen <briantiedjen@gmail.com>
2026-01-04 00:08:46 -0500

    Update outputs.tf with new outputs and value changes
    Added output for Auto Scaling Group name and updated values for ALB logs bucket and web instance ID.

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "VPC_ID" {
   description = "The ID of the VPC created"
 }
 output "vpc_cidr" {
-  value = aws_vpc.vpc.cidr_block
+  value       = aws_vpc.vpc.cidr_block
   description = "CIDR block of the created VPC"
 }
 output "alb_dns_name" {
@@ -18,13 +18,16 @@ output "private_subnet_ids" {
   description = "IDs of the private subnets"
   value       = [for subnet in aws_subnet.private_subnets : subnet.id]
 }
-output "web_instance_id" {
-  description = "ID of the EC2 instance in private subnet"
-  value       = aws_instance.web_server_private.id
+
+output "asg_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.web_server_asg.name
 }
+
+
 output "alb_logs_bucket_name" {
   description = "S3 bucket storing ALB access logs"
-  value       = aws_s3_bucket.backups_bucket.bucket
+  value       = aws_s3_bucket.logs_bucket.bucket
 }
 
 output "alb_url" {
@@ -32,7 +35,7 @@ output "alb_url" {
   description = "URL to access the Application Load Balancer"
 }
 output "target_group_arn" {
-  value = aws_lb_target_group.demo_alb_group.arn
+  value       = aws_lb_target_group.demo_alb_group.arn
   description = "ARN of the ALB target group"
 }
 output "availability_zones" {
