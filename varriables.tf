f48856b22af05b16387b514f4e98cdbbcd36373f
Brian-Tiedjen <Briantiedjen@gmail.com>
2026-01-05 22:39:55 -0500

    Enviorment

--- varriables.tf
+++ varriables.tf
@@ -7,6 +7,9 @@ variable "vpc_name" {
   type    = string
   default = "demo_vpc"
 }
+variable "enviorment" {
+  type = string
+}
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
